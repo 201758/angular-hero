5137703c2c046e6d88ae0f6aed97977051ee29fe
RUI WANG <Rui.Wang1@kyndryl.com>
2022-05-04 17:12:01 +0800

    router

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,7 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {Hero} from '../hero';
 import {HeroService} from '../hero.service';
-import {of} from "rxjs";
+import {MessageService} from '../message.service';
 
 @Component({
   selector: 'app-heroes',
@@ -13,7 +13,7 @@ export class HeroesComponent implements OnInit {
 
   heroes:Hero[]=[];
 
-  constructor(private heroService:HeroService) {
+  constructor(private heroService:HeroService,private messageService:MessageService) {
   }
 
   ngOnInit(): void {
@@ -24,6 +24,8 @@ export class HeroesComponent implements OnInit {
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
+    this.messageService.add(`HeroesComponent: Selected hero ${hero.name}`);
+
   }
   getHeroes ():void{
     this.heroService.getHeroes().subscribe(heroes=>this.heroes=heroes);
